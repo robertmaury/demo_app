71f92565e3905eaa28ab55cf5d762f1d55cb971f
Robert Maury <robert@vitrue.com>
2011-01-05 19:41:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8a23d3f0ea9b5ef8d2b385825c9d35954d631ad7509ebb0320b6597aba113d73ae990b75fbac162e03165d2dffd776cc8b8b1eb22615ab9e6a96ebcadc34147f'
